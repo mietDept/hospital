f8d25f020fe63e12d169e7261c5602c84d6aa969
[salman05-oss] <salmanfardeen1306@gmail.com>
2022-02-28 15:13:51 +0530

    doctor

--- server.js
+++ server.js
@@ -1,127 +1,181 @@
-const express=require('express');
-const app=express();
-const dot=require('dotenv');
+const express = require("express");
+const app = express();
+const { Types } = require("mongoose");
+const { ObjectId } = require("mongoose").Types;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dot = require("dotenv");
+// const salt = await bcrypt.genSalt(8);
 dot.config();
-const mongoose=require('mongoose');//mongodb database
-const bodyparser=require('body-parser');
-app.use(bodyparser.json());//data in json
-app.use(bodyparser.urlencoded({extended:true}));
-const mongourl=process.env.mongo;
-mongoose.connect(mongourl, {useNewUrlParser: true,useUnifiedTopology: true})
-.then(()=>{console.log("connected.....")})//database connection
-.catch((err)=>{console.log(err)})
+const mongoose = require("mongoose"); //mongodb database
+const bodyparser = require("body-parser");
+app.use(bodyparser.json()); //data in json
+app.use(bodyparser.urlencoded({ extended: true }));
+const mongourl = process.env.mongo;
+mongoose
+  .connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected.....");
+  }) //database connection
+  .catch((err) => {
+    console.log(err);
+  });
 //hospital list schema
-const hospital=new mongoose.Schema({
-    name:String,
-    registerNo:String,
-    certNo:String,
-    gstNo:String,
-    email:String,
-    password:String,
-    phone:Number,
-    approvalStatus:String,
-    place:String,
-    branch:String,
-    speacility:String,
-   
+const hospital = new mongoose.Schema({
+  name: String,
+  registerNo: String,
+  certNo: String,
+  gstNo: String,
+  email: String,
+  password: String,
+  phone: String,
+  approvalStatus: String,
+  place: String,
+  branch: String,
+  speacility: String,
 });
 //patient list schema
-const patient=new mongoose.Schema({
-    Name:String,
-    AdhaarNo:Number,
-    PhoneNumber:Number,
-    email:String,
-    password:String,
-    District:String,
-    state:String,
-    address:String,
-   
+const patient = new mongoose.Schema({
+  Name: String,
+  AdhaarNo: Number,
+  PhoneNumber: String,
+  email: String,
+  password: String,
+  District: String,
+  state: String,
+  address: String,
 });
 //doctor list schema
-const doctor=new mongoose.Schema({
-    Name:String,
-    speciality:String,
-    hospital:mongoose.Schema.Types.ObjectId,
-    
-})
-const Patient=new mongoose.model("patient",patient);//patient model
-const Hospital=mongoose.model("hospitals",hospital);//hospital model
-const Doctor=mongoose.model("doctors",doctor);//doctor model
+const doctor = new mongoose.Schema({
+  Name: String,
+  speciality: String,
+  hospital: mongoose.Schema.Types.ObjectId,
+});
+const Patient = new mongoose.model("patient", patient); //patient model
+const Hospital = mongoose.model("hospitals", hospital); //hospital model
+const Doctor = mongoose.model("doctors", doctor); //doctor model
+
+const auth = (req, res, next) => {
+  const header = req.headers["authorization"];
+  const token = header && header.split(" ")[1];
+  console.log(token);
+  jwt.verify(token, process.env.TOKEN_AUTH, (err, user) => {
+    if (err) {
+      return err;
+    }
+    req.user = user;
+    next();
+  });
+};
 //home page
-app.get('/',(req,res)=>{
-    res.send("home page");
-})
+app.get("/", (req, res) => {
+  res.send("home page");
+});
 
 //patient login
-app.get('/patientlogin',(req,res)=>{
-    Patient.find({email:req.body.email},(err,resp)=>{
-        if(resp.password==req.body.password){
-            res.send({message:"logged in"})
-        }
-        else{
-            res.send({message:"not logged in"})
-        }
-    })
-})
+app.get("/patientlogin", (req, res) => {
+  Patient.findOne({ email: req.body.email }, async (err, resp) => {
+    if (resp != undefined) {
+      // console.log(resp);
+      // console.log(req.user);
+      // console.log(req.body.password, resp.password);
+      var verification = await bcrypt.compare(req.body.password, resp.password);
+      if (verification) {
+        const token = jwt.sign({ id: resp._id }, process.env.TOKEN_AUTH, {
+          expiresIn: "1d",
+        });
+        res.send({ message: "sign in successful", data: resp, token });
+      } else {
+        res.send({ message: "Invalid Password" });
+      }
+    } else {
+      res.send({ message: "user does not exist" });
+    }
+  });
+});
 //hospital login
-app.get('/hospitallogin',(req,res)=>{
-    Hospital.find({email:req.body.email},(err,re)=>{
-        if(re.password==req.body.password){
-            res.send({message:"logged in"})
-        }
-        else{
-            res.send({message:"not logged in"})
-        }
-    })
-    
+app.get("/hospitallogin", async (req, res) => {
+  Hospital.findOne({ email: req.body.email }, async (err, re) => {
+    if (re != undefined) {
+      console.log(re);
+      console.log(req.body.password, re.password);
+      var verification = await bcrypt.compare(req.body.password, re.password);
+      console.log(verification);
+      if (verification) {
+        const token = jwt.sign({ id: re._id }, process.env.TOKEN_AUTH, {
+          expiresIn: "1d",
+        });
+        res.send({ message: "sign in successful", data: re, token });
+      } else {
+        res.send({ message: "Invalid Password" });
+      }
+    } else {
+      res.send({ message: "user does not exist" });
+    }
+  });
 });
 //hospital register
-app.post('/register',(req,res)=>{
-    var register=Hospital({
-        name:req.body.name,
-        registerNo:req.body.registerno,
-        certNo:req.body.certno,
-        gstNo:req.body.gstno,
-        email:req.body.email,
-        password:pass.generate(req.body.password),
-        phone:req.body.phone,
-        approvalStatus:req.body.approval,
-        place:req.body.place,
-        branch:req.body.branch,
-        speacility:req.body.speciality,
-    });
-    register.save();
-    res.send({message:"hospital account created"});
-    
-});//workflow
+app.post("/register", async (req, res) => {
+  var register = Hospital({
+    name: req.body.name,
+    registerNo: req.body.registerno,
+    certNo: req.body.certno,
+    gstNo: req.body.gstno,
+    email: req.body.email,
+    password: await bcrypt.hash(req.body.password, await bcrypt.genSalt(10)),
+    phone: req.body.phone,
+    approvalStatus: req.body.approval,
+    place: req.body.place,
+    branch: req.body.branch,
+    speacility: req.body.speciality,
+  });
+  register.save();
+  res.send({ message: "hospital account created" });
+}); //workflow
 //patient register
-app.post('/patientregister',(req,res)=>{
-    var sav=Patient({
-        Name:req.body.name,
-        AdhaarNo:req.body.adhaarno,
-        PhoneNumber:req.body.phone,
-        email:req.body.email,
-        password:pass.generate(req.body.password),
-        District:req.body.district,
-        state:req.body.state,
-        address:req.body.address,
-     });
-    sav.save();
-    res.send("patient created");
+app.post("/patientregister", async (req, res) => {
+  var sav = Patient({
+    Name: req.body.name,
+    AdhaarNo: req.body.adhaarno,
+    PhoneNumber: req.body.phone,
+    email: req.body.email,
+    password: await bcrypt.hash(req.body.password, await bcrypt.genSalt(10)),
+    District: req.body.district,
+    state: req.body.state,
+    address: req.body.address,
+  });
+  sav.save();
+  res.send({ message: "patient created" });
 });
 //
-app.post('/doctors',(req,res)=>{
-var doc=Doctor({
-    Name:req.body.name,
-    speciality:req.body.speacility,
-    hospital:req.body.hospital,
-})
-doc.save()
-.then(res.send("doctor created"))
-.catch((err)=>{console.log(err)})
+app.post("/doctors", (req, res) => {
+  var doc = Doctor({
+    Name: req.body.name,
+    speciality: req.body.speacility,
+    hospital: req.body.hospital,
+  });
+  doc
+    .save()
+    .then(res.send("doctor created"))
+    .catch((err) => {
+      console.log(err);
+    });
 });
-app.get("/doctor",(req,res)=>{
-    Doctor.find({hospital:req.body.hospitalid},(err,resd)=>{
-        res.send({"doctor":resd})})
-})
-app.listen(9000);
+app.get("/doctor", (req, res) => {
+  Doctor.find(
+    { hospital: Types.ObjectId(req.body.hospitalid) },
+    (err, resd) => {
+      res.send({ doctor: resd });
+    }
+  );
+});
+
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+// server listening to the port
+app.listen(port, host, () =>
+  console.log(
+    `server is successfully listenning at host: ${host}, port: ${port}`
+  )
+);
